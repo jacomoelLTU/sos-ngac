98d13f448f8752d01f49bbf84435ee958861fcb3
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-08 13:17:22 +0100

    Fixed uploading issue via curl command.

--- sos-ngac/sos-ngac-consumer/src/main/java/ai/aitia/sos_ngac/resource_consumer/ConsumerMain.java
+++ sos-ngac/sos-ngac-consumer/src/main/java/ai/aitia/sos_ngac/resource_consumer/ConsumerMain.java
@@ -146,10 +146,17 @@ public class ConsumerMain implements ApplicationRunner {
 			String applicationType ="Content-Type: application/json";
 			String jsonArr = "{  \"address\": \"localhost\",  \"metadata\": {    \"SensorType\": \"Temp\",    \"manF\": \"BOOSCH\",    \"additionalProp3\": \"string\"  },  \"port\": 0,  \"systemName\": \"Sensor11\"}";
 			
+
+			/*					THIS IS JUST WORKING COMMANDS THAT IS HARD CODED AND WORKING			 */
+
 			//curl -X POST "https://localhost:8443/serviceregistry/mgmt/systems" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \"address\": \"localhost\",  \"metadata\": {    \"SensorType\": \"Temp\",    \"manF\": \"BOOSCH\",    \"additionalProp3\": \"string\"  },  \"port\": 0,  \"systemName\": \"Sensor1\"}"
 			//ProcessBuilder pb = new ProcessBuilder("curl", "-X", "POST", "http://localhost:8443/serviceregistry/mgmt/systems", "-H", "accept: application/json", "-H", "Content-Type: application/json", "-d", "{  \"address\": \"localhost\",  \"metadata\": {    \"SensorType\": \"Temp\",    \"manF\": \"BOOSCH\",    \"additionalProp3\": \"string\"  },  \"port\": 0,  \"systemName\": \"Sensor9\"}");
 			//curl -X GET "http://localhost:8443/serviceregistry/mgmt/systems/66" -H  "accept: */*"
 			//ProcessBuilder pb = new ProcessBuilder("curl", "-X", "GET", "http://localhost:8443/serviceregistry/mgmt/systems/66", "-H", "accept: */*");
+			
+			/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+			
+			
 			ProcessBuilder pb = new ProcessBuilder("curl", "-X", "POST", hostName, "-H", application, "-H", applicationType, "-d", jsonArr);
 			
 			
